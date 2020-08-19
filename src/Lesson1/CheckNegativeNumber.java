980c4b6fdeb53650349f45494f8bb7fba58a865d
apanyukam <apanyuk@gmail.com>
2020-08-19 20:48:44 +0300

    Homework lesson1, task6

--- src/Lesson1/CheckNegativeNumber.java
+++ src/Lesson1/CheckNegativeNumber.java
@@ -3,6 +3,8 @@ package Lesson1;
 Задача 5: Написать метод, которому в качестве параметра передается целое число,
 метод должен напечатать в консоль положительное ли число передали, или отрицательное;
 Замечание: ноль считаем положительным числом.
+Задача 6: Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true,
+если число отрицательное;
  */
 import java.util.Scanner;
 
@@ -14,14 +16,21 @@ public class CheckNegativeNumber {
         System.out.println("Введите целое число: ");
         int a = scanner.nextInt();
         System.out.println("Вы ввели " + CheckNumber(a));
+        System.out.println(CheckNumberAndMessage(a));
 
 
     }
     private static String CheckNumber(int a){
         String message;
-        if (a > 0){
+        if (a >= 0){
             message = "положительное число";
         } else message = "отрицательное число";
         return message;
+    }
+
+    private static boolean CheckNumberAndMessage(int a){
+        boolean result;
+        result = a < 0;
+        return result;
     }
 }
