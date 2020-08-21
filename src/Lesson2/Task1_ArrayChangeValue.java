de1760a488c5874960e2b96131ebd57102e496f2
apanyukam <apanyuk@gmail.com>
2020-08-21 17:15:47 +0300

    Task1_ArrayChangeValue added

---
+++ src/Lesson2/Task1_ArrayChangeValue.java
@@ -0,0 +1,12 @@
+package Lesson2;
+/*
+Задать целочисленный массив, состоящий из элементов 0 и 1.
+Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+С помощью цикла и условия заменить 0 на 1, 1 на 0;
+ */
+
+public class Task1_ArrayChangeValue {
+    public static void main(String[] args) {
+        int[] firstArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+    }
+}
